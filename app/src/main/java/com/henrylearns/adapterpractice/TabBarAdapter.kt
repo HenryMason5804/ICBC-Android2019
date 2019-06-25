c4ab6bb03514329a1ff2e36dce5157bc92acaadc
Henry Mason <henryjamesmason@gmail.com>
2019-06-25 14:04:28 -0400

    updated adapter

--- app/src/main/java/com/henrylearns/adapterpractice/TabBarAdapter.kt
+++ app/src/main/java/com/henrylearns/adapterpractice/TabBarAdapter.kt
@@ -1,5 +1,6 @@
 package com.henrylearns.adapterpractice
 
+import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
@@ -10,13 +11,25 @@ class TabBarAdapter (fragmentManager: FragmentManager) : FragmentPagerAdapter(fr
 
         override fun getItem(position: Int): Fragment {
             return when (position) {
-                0 -> SponsorFragment()
-                1 -> SponsorFragment()
-                else -> SponsorFragment()
-            }
+                0 ->{ Log.d("getItemCheck","Went to 0")
+                    return EventFragment()}
+                else -> {
+                    Log.d("getItemCheck","Went to 1")
+                    SponsorFragment()}
+
+                }
+
         }
 
         override fun getCount(): Int {
             return 2
         }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when (position){
+            1-> "Sponsors"
+            2->"Events"
+            else->{return "Events"}
+        }
+    }
     }
