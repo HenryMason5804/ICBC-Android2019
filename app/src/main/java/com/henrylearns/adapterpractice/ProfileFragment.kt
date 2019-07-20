de16831bf1adc1a173d0fed392849d7f0b98adca
Henry Mason <henryjamesmason@gmail.com>
2019-07-20 15:56:33 -0400

    Created skeleton for Profile

--- app/src/main/java/com/henrylearns/adapterpractice/ProfileFragment.kt
+++ app/src/main/java/com/henrylearns/adapterpractice/ProfileFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -14,9 +17,16 @@ class ProfileFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        val view=inflater.inflate(R.layout.fragment_profile,container,false)
+        initRecyclerAdapter(view)
+        return view}
 
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
-
-
+fun initRecyclerAdapter(view:View){
+    val recyclerView=view.findViewById<RecyclerView>(R.id.execTeamRecycler)
+    val manager = LinearLayoutManager(view.context)
+    recyclerView.setHasFixedSize(true)
+    recyclerView.layoutManager = manager
+    val myAdapter=profileAdapter(view.context)
+    recyclerView.adapter=myAdapter
+}
 }
